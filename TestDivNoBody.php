9143a4b82101373421a61cbef13944f631f66e33
Scott Richards <scott.srichards@gmail.com>
2012-07-13 15:34:24 -0700

    Working on navigation with database queries.

---
+++ TestDivNoBody.php
@@ -0,0 +1,99 @@
+<?php require_once('../Connections/projector.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$currentPage = $_SERVER["PHP_SELF"];
+
+$maxRows_Recordset1 = 3;
+$pageNum_Recordset1 = 0;
+if (isset($_GET['pageNum_Recordset1'])) {
+  $pageNum_Recordset1 = $_GET['pageNum_Recordset1'];
+}
+$startRow_Recordset1 = $pageNum_Recordset1 * $maxRows_Recordset1;
+
+mysql_select_db($database_projector, $projector);
+$query_Recordset1 = "SELECT * FROM projectGallery";
+$query_limit_Recordset1 = sprintf("%s LIMIT %d, %d", $query_Recordset1, $startRow_Recordset1, $maxRows_Recordset1);
+$Recordset1 = mysql_query($query_limit_Recordset1, $projector) or die(mysql_error());
+$row_Recordset1 = mysql_fetch_assoc($Recordset1);
+
+if (isset($_GET['totalRows_Recordset1'])) {
+  $totalRows_Recordset1 = $_GET['totalRows_Recordset1'];
+} else {
+  $all_Recordset1 = mysql_query($query_Recordset1);
+  $totalRows_Recordset1 = mysql_num_rows($all_Recordset1);
+}
+$totalPages_Recordset1 = ceil($totalRows_Recordset1/$maxRows_Recordset1)-1;
+
+$queryString_Recordset1 = "";
+if (!empty($_SERVER['QUERY_STRING'])) {
+  $params = explode("&", $_SERVER['QUERY_STRING']);
+  $newParams = array();
+  foreach ($params as $param) {
+    if (stristr($param, "pageNum_Recordset1") == false && 
+        stristr($param, "totalRows_Recordset1") == false) {
+      array_push($newParams, $param);
+    }
+  }
+  if (count($newParams) != 0) {
+    $queryString_Recordset1 = "&" . htmlentities(implode("&", $newParams));
+  }
+}
+$queryString_Recordset1 = sprintf("&totalRows_Recordset1=%d%s", $totalRows_Recordset1, $queryString_Recordset1);
+?>
+
+<table width="200" border="0" cellspacing="1">
+  <tr>
+    <td><a href="<?php printf("%s?pageNum_Recordset1=%d%s", $currentPage, max(0, $pageNum_Recordset1 - 1), $queryString_Recordset1); ?>">Previous</a></td>
+    <td><a href="<?php printf("%s?pageNum_Recordset1=%d%s", $currentPage, min($totalPages_Recordset1, $pageNum_Recordset1 + 1), $queryString_Recordset1); ?>">Next</a></td>
+  </tr>
+</table>
+<p>Page <?php echo ($pageNum_Recordset1 + 1) ?> of <?php echo ($totalPages_Recordset1 + 1) ?></p>
+<?php
+	$columnNum = 1; 
+	do { 
+		print "<div id=\"GalleryColumn" .  $columnNum . "Div\" >";
+		print "\n\t<div id=\"GalleryMedia\">";
+		print "\n\t\t<img src=\"" . $row_Recordset1['SrcImg'] . "\" width=\"300\" height=\"200\">";
+		print "\n\t</div>";
+		print "\n\t<h1>" . $row_Recordset1['Name']. "</h1>";
+		print "\n\t<p>" . $row_Recordset1['Description'] . "</p>";
+		print "\n\t<p><strong>" . $row_Recordset1['Subject'] . "</p>";
+		print "\n\t<p><strong>" . $row_Recordset1['Grade'] . "</strong></p>";
+		print "\n\t<p><strong>" . $row_Recordset1['Duration'] . "</strong></p>";
+		print "\n</div>\n";
+		if (++$columnNum > 3)
+				$columnNum = 1;
+	} while ($row_Recordset1 = mysql_fetch_assoc($Recordset1));
+	
+	mysql_free_result($Recordset1);
+?>
