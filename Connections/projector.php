162b2004e2377b260197e27b5f9edd57bcdc293e
Scott Richards <scott.srichards@gmail.com>
2012-09-11 15:22:15 -0700

    trying to connect to Amazon RDS

--- Connections/projector.php
+++ Connections/projector.php
@@ -2,9 +2,9 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_projector = "localhost";
+$hostname_projector = "projector.cbz3amb2qamj.us-east-1.rds.amazonaws.com";
 $database_projector = "projector";
-$username_projector = "srichards";
-$password_projector = "Diamond";
+$username_projector = "pearsonf";
+$password_projector = "Poseidon";
 $projector = mysql_pconnect($hostname_projector, $username_projector, $password_projector) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
