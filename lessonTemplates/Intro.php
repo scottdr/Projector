9bd703223432239d7b88e61e897390e9bbbf95d5
Scott Richards <scott.srichards@gmail.com>
2012-09-11 14:28:21 -0700

    Adding Intro Template

---
+++ lessonTemplates/Intro.php
@@ -0,0 +1,8 @@
+<div id="ChallengeWrapper">
+  <div>
+    <div id="ChallengeImageLeftTop"> <?php GenerateImageTag(0); ?></div>
+    <div id="ChallengeImageRightTop"> <?php GenerateImageTag(1); ?></div>
+    <div id="ChallengeImageLeftBottom"> <?php GenerateImageTag(2); ?></div>
+    <div id="ChallengeTextRightBottom"><?php echo $row_StepQuery['Text']; ?></div>
+  </div>
+</div>
