f90ec65e292ec23a1333f980e1ae87afd1f142ae
Tanya Heins <tanya.numen@gmail.com>
2012-09-26 19:14:34 -0400

    Misc. edits.  Project Details css changes are the highest risk.

--- lessonTemplates/Plan.php
+++ lessonTemplates/Plan.php
@@ -1,5 +1,5 @@
 <div id="FullWidthWrapper">
-  <div id="PCRPTwoColumnLeft"> <img src="_images/challenge/pcrp-process.png"/> </div>
+  <div id="PCRPTwoColumnLeft"> <img src="_images/challenge/plan.png"/> </div>
   <div id="PCRPTwoColumnRight">
     <h2><?php echo $row_StepQuery['Title']; ?></h2>
     <div id="Text"><?php echo $row_StepQuery['Text']; ?></div>
