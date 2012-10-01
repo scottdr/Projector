dc13428f64cd3d831d649b8cf003b45043c051dd
Scott Richards <scott.srichards@gmail.com>
2012-10-01 15:35:47 -0700

    Move the dev database instance to the west coast

--- Connections/projector_dev.php
+++ Connections/projector_dev.php
@@ -2,7 +2,7 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_projector = "projector.cbz3amb2qamj.us-east-1.rds.amazonaws.com";
+$hostname_projector = "projectordev.cy8aybqmalrv.us-west-1.rds.amazonaws.com";
 $database_projector = "projector";
 $username_projector = "pearsonf";
 $password_projector = "Poseidon";
