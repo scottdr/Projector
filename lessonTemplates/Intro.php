7dae0c85d574e4320e539eea5088ced98445d564
Scott Richards <scott.srichards@gmail.com>
2012-10-02 14:16:13 -0700

    Changed Intro to add Intro Challenge Slide Show.

--- lessonTemplates/Intro.php
+++ lessonTemplates/Intro.php
@@ -1,8 +1,43 @@
-<div id="ChallengeWrapper">
-  <div>
-    <div id="ChallengeImageLeftTop"> <?php GenerateImageTag(0); ?></div>
-    <div id="ChallengeImageRightTop"> <?php GenerateImageTag(1); ?></div>
-    <div id="ChallengeImageLeftBottom"> <?php GenerateImageTag(2); ?></div>
-    <div id="ChallengeTextRightBottom"><?php echo $row_StepQuery['Text']; ?></div>
+<div id="ChallengeWrapper"> <!-- ChallengeWrapper. Constrained content area for presentation elements. -->
+    
+  <div id="ChallengeTitle">
+    <img src="assets/images/intrologo.png" width="175" height="87" alt="Projector Logo" class="ProjectorLogo" />
+    <h1 class="ProjectorTitle">Pearson Foundation Learning Labs</h1>
+    <h2 id="ChallengeTitleProject" class="ProjectTitle">Sculpture, Drama and Dialogue</h2>
+    <h3 id="ChallengeTitleAuthor" class="ProjectAuthor">by Jeane Muzi</h3>
+    <img id="ChallengeTitleBtn" src="assets/images/challengeintro_arrow.png" width="60" height="60" />
   </div>
-</div>
+
+  <div id="ChallengeGroupA" class="landscapex3">
+    <div id="ChallengeImageA1" class="challengeImage1">
+      <img id="ChallengeImageA1Img" src="" title="Stringed Instrument" alt="Stringed Instrument">
+    </div>
+    <div id="ChallengeImageA2" class="challengeImage2">
+      <img id="ChallengeImageA2Img" src="" title="Lorem ipsum dolor sit amet" alt="Lorem ipsum dolor sit amet">
+    </div>
+    <div id="ChallengeImageA3" class="challengeImage3">
+      <img id="ChallengeImageA3Img" src="" title="" alt="">
+    </div>
+  </div>
+  
+  <div id="ChallengeGroupB" class="landscapex3">
+    <div id="ChallengeImageB1" class="challengeImage1">
+      <img id="ChallengeImageB1Img" src="" title="" alt="">
+    </div>
+    <div id="ChallengeImageB2" class="challengeImage2">
+      <img id="ChallengeImageB2Img" src="" title="" alt="">
+    </div>
+    <div id="ChallengeImageB3" class="challengeImage3">
+      <img id="ChallengeImageB3Img" src="" title="" alt="">
+    </div>
+  </div>
+  
+  <div id="ChallengeText">
+  </div>
+  
+  <div id="ChallengePaused">
+    <img id="ChallengePausedImg" src="assets/images/challengeintro_arrow.png" width="60" height="60" />
+  </div>
+  
+</div> <!-- ChallengeWrapper Ends --> 
+<div id="challengeAudioPlayer" class="jp-jplayer"></div>
