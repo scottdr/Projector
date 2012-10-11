34d8ccf231c7b8659358fcacd44c10132136a563
Scott Richards <scott.srichards@gmail.com>
2012-10-10 18:22:30 -0700

    Made an updated read replica of the stage database.

--- Connections/projector_production.php
+++ Connections/projector_production.php
@@ -2,7 +2,7 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_projector = "productionreadreplica.cy8aybqmalrv.us-west-1.rds.amazonaws.com";
+$hostname_projector = "productionreplicaoct10.cy8aybqmalrv.us-west-1.rds.amazonaws.com";
 $database_projector = "projector";
 $username_projector = "pearsonf";
 $password_projector = "Poseidon";
