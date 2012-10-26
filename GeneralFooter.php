ce2d4603f6f41a7158ece3011d8f2a81129dc8b8
Scott Richards <scott.srichards@gmail.com>
2012-10-26 10:58:02 -0700

    Add release # to footer.  Comment it out on the production server.

--- GeneralFooter.php
+++ GeneralFooter.php
@@ -1,8 +1,16 @@
+ <?php include('Globals.php') ?>
+ <?php include('VersionNumber.php') ?>
+ 
             <div id="GeneralFooterDiv">
                 <hr/>
-                <p>© Pearson Foundation 2012 | <a href="mailto:labs@pearsonfoundation.org">Contact</a> | <a href="TermsConditions.php">Terms and Conditions</a></p>
+                <p>&copy; Pearson Foundation 2012 | <a href="mailto:labs@pearsonfoundation.org">Contact</a> | <a href="TermsConditions.php">Terms and Conditions</a>
+                <?php if (!$PROJECTOR['editMode']) echo "<!-- "; ?> 
+								<?php echo "&nbsp;Release: #" . $PROJECTOR['versionNumber']; ?>
+                <?php if (!$PROJECTOR['editMode']) echo "--> " ?> 
+                </p>
                 <a href="http://www.teachingawards.com/home" target="_blank"><img src="_images/logo_teachingawards.gif" alt="Pearson Teaching Awards"></a>
                 <!--a href="http://www.si.edu" target="_blank"><img src="_images/logo_smithsonian.gif" alt="Smithsonian"></a-->
-                <a href="http://www.pearsonfoundation.org" target="_blank"><img src="_images/logo_pearsonfound.gif" alt="Pearson Teaching Awards"></a>
+                <a href="http://www.pearsonfoundation.org" target="_blank"><img src="_images/logo_pearsonfound.gif" alt="Pearson Foundation"></a>
                 <a href="http://www.nationalmockelection.org" target="_blank"><img src="_images/logo_myvoice.gif" alt="My Voice My Election"></a>
+               
            </div>
