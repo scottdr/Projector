e0eeb32998e15cef984260540e7e106c4b219934
Scott Richards <scott.srichards@gmail.com>
2012-11-02 10:23:42 -0700

    Adding New template with Icon

---
+++ lessonTemplates/IconLeft.php
@@ -0,0 +1,7 @@
+<div id="FullWidthWrapper">
+  <div id="PCRPTwoColumnLeft"><?php GenerateImageTag(0); ?></div>
+  <div id="PCRPTwoColumnRight">
+    <h2><?php echo $row_StepQuery['Title']; ?></h2>
+    <div id="Text"><?php echo $row_StepQuery['Text']; ?></div>
+  </div>
+</div>
