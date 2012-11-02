11dd97400c73bf1520afa40c8542c2c0933fe56d
Scott Richards <scott.srichards@gmail.com>
2012-11-02 10:57:12 -0700

    Reduce max image size

--- lessonTemplates/IconLeft.php
+++ lessonTemplates/IconLeft.php
@@ -1,5 +1,5 @@
 <div id="FullWidthWrapper">
-  <div id="PCRPTwoColumnLeft"><?php GenerateImageTag(0); ?></div>
+  <div id="PCRPTwoColumnLeft" style="margin-right:20px; max-width:300px; max-height:300px;"><?php GenerateImageTag(0); ?></div>
   <div id="PCRPTwoColumnRight">
     <h2><?php echo $row_StepQuery['Title']; ?></h2>
     <div id="Text"><?php echo $row_StepQuery['Text']; ?></div>
