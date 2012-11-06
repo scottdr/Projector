421fe4d2a032996784d6e8b0e024f7acd7554f1e
Scott Richards <scott.srichards@gmail.com>
2012-11-06 14:28:56 -0800

    Adding back removal of slide show.

--- RibbonDynamicContent.php
+++ RibbonDynamicContent.php
@@ -31,7 +31,7 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
 }
 }
 
-$StepNumber = 1;
+$SelectedStepNumber = 1;
 
 $ProjectId = 1;
 if (isset($_GET['ProjectId'])) {
@@ -42,12 +42,22 @@ mysql_select_db($database_projector, $projector);
 $query_stepsRecordset = sprintf("SELECT Steps.Id, ProjectId, SortOrder, TemplateName, LessonName, RoutineId, RoutineName, CSSName FROM Steps, Routines WHERE ProjectId = %s AND Steps.RoutineId = Routines.Id ORDER BY SortOrder",$ProjectId);
 $stepsRecordset = mysql_query($query_stepsRecordset, $projector) or die(mysql_error());
 $row_stepsRecordset = mysql_fetch_assoc($stepsRecordset);
+$subtractSlideShowStep = 0;
+if ($PROJECTOR['disableSlideShow']) {		// check if we have disabled the slide show feature and if so remove the first step
+	// if the first step is using the Intro template and we are hiding
+	if ($row_stepsRecordset['TemplateName'] == 'Intro.php') {	
+		$subtractSlideShowStep = 1;
+		$row_stepsRecordset = mysql_fetch_assoc($stepsRecordset);
+	}
+}
+
 $totalRows_stepsRecordset = mysql_num_rows($stepsRecordset);
 $currentRoutineName = "";
 $stepsArray = array();
 $rowNumber = 0;
 do {
 	if (isset($row_stepsRecordset)) {
+		$rowStepNumber = $row_stepsRecordset['SortOrder'] - $subtractSlideShowStep;
 		if ($row_stepsRecordset['CSSName'] != $currentRoutineName) {
 			if ($currentRoutineName != '')
 				print "\n</div>\n"; // close off previous div when we need to
@@ -58,15 +68,15 @@ do {
 			$currentRoutineName = $row_stepsRecordset['CSSName'];
 		}
 		
-		print "\n  " . '<div class="ribbon' . $row_stepsRecordset['CSSName'] . 'ColumnWrap" data-type="wrapper" data-number="' . $row_stepsRecordset['SortOrder'] . '" data-id="' . $row_stepsRecordset['Id'] . '" >'; 			// <div class="ribbonChallengeColumnWrap">
-		if ($StepNumber == $row_stepsRecordset['SortOrder']) {	// if the step number is the currently selected one set class to BottomCurrent
+		print "\n  " . '<div class="ribbon' . $row_stepsRecordset['CSSName'] . 'ColumnWrap" data-type="wrapper" data-number="' . $rowStepNumber . '" data-id="' . $row_stepsRecordset['Id'] . '" ' . 'ontouchstart="touchStart(event,\'step\');" ontouchend="touchEnd(event);" ontouchmove="touchMove(event);" ontouchcancel="touchCancel(event);"' . '" >'; 			// <div class="ribbonChallengeColumnWrap">
+		if ($SelectedStepNumber == $rowStepNumber) {	// if the step number is the currently selected one set class to BottomCurrent
 			print "\n    " . '<div class="ribbon' . $row_stepsRecordset['CSSName'] . 'BottomCurrent" data-type="bottom">'; 	//   <div class="ribbonChallengeBottomCurrent">
 		} else
 			print "\n    " . '<div class="ribbon' . $row_stepsRecordset['CSSName'] . 'Bottom" data-type="bottom">'; 	//   <div class="ribbonChallengeBottomCurrent">		
-		print "\n      " . '<p class="' . $row_stepsRecordset['CSSName'] . 'Number">' . $row_stepsRecordset['SortOrder'] . '</p>'; // <p class="ChallengeNumber">1</p>
+		print "\n      " . '<p class="' . $row_stepsRecordset['CSSName'] . 'Number">' . $rowStepNumber . '</p>'; // <p class="ChallengeNumber">1</p>
 		print "\n      " . '<h2>' . $row_stepsRecordset['LessonName'] . '</h2>';  // <h2>Challenge Video</h2>
 		print "\n    " . '</div>';
-		if ($StepNumber == $row_stepsRecordset['SortOrder'])
+		if ($SelectedStepNumber == $rowStepNumber)
 			print "\n    " . '<div class="ribbon' . $row_stepsRecordset['CSSName'] . 'Selector visibleStyle" data-type="selector"> </div>'; 
 		else
 			print "\n    " . '<div class="ribbon' . $row_stepsRecordset['CSSName'] . 'Selector hiddenStyle" data-type="selector"> </div>';
