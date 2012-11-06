04ac43fd6fb65427f7f8fc4a48024f5f59a14ff6
Scott Richards <scott.srichards@gmail.com>
2012-11-06 14:21:09 -0800

    Put javascript variable to turn off slide show in the .js file instead of Globals.php

--- Globals_production.php
+++ Globals_production.php
@@ -2,12 +2,3 @@
 $PROJECTOR['editMode'] = false;
 $PROJECTOR['disableSlideShow'] = true;
 ?>
-<?php if ($PROJECTOR['disableSlideShow']) : ?>
-<script type="text/javascript">
-	var disableSlideShow = true;	// disable the slide show until we polish it
-</script>
-<?php else: ?>
-<script type="text/javascript">
-	var disableSlideShow = false;	// enable the slide show
-</script>
-<?php endif; ?>
