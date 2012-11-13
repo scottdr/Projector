cf48ddf1f39d4e0daa56191bd69587d6d004bf6f
Scott Richards <scott.srichards@gmail.com>
2012-11-13 14:47:56 -0800

    Testing pulling down changes.

---
+++ tools/test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<?php
+
+$cmd = "cd /var/www; ";
+# $cmd .= "ls -l";
+$cmd .= "sudo git pull";
+echo "shell_exec(" . $cmd . ")<br />";
+$result = shell_exec($cmd);
+echo "result: " . $result;
+?>
+</body>
+</html>
