a0d7c2b1d120da9169e17092e1c3af09333e0834
Stephen Downs <steve@plasticbrain.com>
2012-12-13 15:54:22 -0800

    Candyfloss: Resource Detail page base functionality.

--- candyfloss/ResourceDetail.php
+++ candyfloss/ResourceDetail.php
@@ -1,3 +1,61 @@
+<?php require_once('../Connections/projector.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$colname_Resource = "-1";
+if (isset($_GET['Id'])) {
+  $colname_Resource = $_GET['Id'];
+}
+mysql_select_db($database_projector, $projector);
+$query_Resource = sprintf("SELECT * FROM CF_Resources WHERE Id = %s", GetSQLValueString($colname_Resource, "int"));
+$Resource = mysql_query($query_Resource, $projector) or die(mysql_error());
+$row_Resource = mysql_fetch_assoc($Resource);
+$totalRows_Resource = mysql_num_rows($Resource);
+
+// Age string:
+$ageStart = $row_Resource['AgeStart'];
+$ageEnd = $row_Resource['AgeEnd'];
+$ageRangeStr = "all ages";
+if ($ageStart && $ageEnd) {
+	$ageRangeStr = $ageStart . '-' . $ageEnd . ' years';
+} else {
+	if ($ageEnd) {
+		$ageRangeStr = 'up to ' . $ageEnd . ' years old';
+	} else if ($ageStart) {
+		$ageRangeStr = $ageStart . ' years and older';
+	}
+}
+
+?>
 <!doctype html>
 <html>
     <head>
@@ -21,8 +79,8 @@
     <!-- Header Starts -->
         <div class="navbar navbar-inverse navbar-fixed-top">
             <div class="navbar-inner">
-                <div class="container-fluid">
-                 <a class="brand" href="#"><img src="img/headerlogo.png">Common Core</a>
+	            <div class="container-fluid">
+                 <a class="brand" href="ResourcesViewAll.php" style="background-color:pink">Candy Floss</a>
                 </div>
             </div>
         </div>
@@ -54,19 +112,20 @@
             </section>
             
             <section class="row-fluid" style="background-color:#FFF;">
-       	    <img class="span7" src="img/static-content/powersoften.jpg">
+            <img class="span7" <?php echo $row_Resource['ImageLarge']?'':'style="opacity:0;"'; ?> src="<?php echo $row_Resource['ImageLarge']?$row_Resource['ImageLarge']:'img/static-content/powersoften.jpg'; ?>">
             <div class="span5" style="padding-top:20px; padding-right:20px;">
-            	<a href="#" target="_blank" class="btn btn-small btn-success" style="float:right; clear:none;"><i class="icon-globe icon-white"></i> Open web link</a>
-                <h4 class="descriptionSectionHeading">Title of the resource</h4>
-                <p class="descriptionCopyBody">Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.				</p>
+            	<a href="<?php echo $row_Resource['URL']; ?>" target="_blank" class="btn btn-small btn-success" style="float:right; clear:none;"><i class="icon-globe icon-white"></i> Open web link</a>
+                <h4 class="descriptionSectionHeading"><?php echo $row_Resource['Name']; ?></h4>
+                
+                <p class="descriptionCopyBody"><?php echo $row_Resource['AboutDetail']; ?></p>
                 <h5 class="descriptionCopyHeading">Publisher</h5>
-                <p class="descriptionCopyBody">Publisher name</p>
+                <p class="descriptionCopyBody"><?php echo $row_Resource['Publisher']; ?></p>
                 <h5 class="descriptionCopyHeading">Grade level</h5>
                 <p class="descriptionCopyBody">10</p>
                 <h5 class="descriptionCopyHeading">Age suitability</h5>
-                <p class="descriptionCopyBody">5-99 years</p>
+                <p class="descriptionCopyBody"><?php echo $ageRangeStr; ?></p>
                 <h5 class="descriptionCopyHeading">Media Type</h5>
-                <p class="descriptionCopyBody">Simulation</p>
+                <p class="descriptionCopyBody"><?php echo $row_Resource['MediaType']; ?></p>
             </div>
             </section>
             
@@ -80,25 +139,23 @@
             <section class="row-fluid">
                 <div class="span3">
                     <h5 class="descriptionCopyHeading">Author</h5>
-                    <p class="descriptionCopyBody">Author name</p>
+                    <p class="descriptionCopyBody"><?php echo $row_Resource['Author']?$row_Resource['Author']:'--'; ?></p>
                     <h5 class="descriptionCopyHeading">Language</h5>
-                    <p class="descriptionCopyBody">Language</p>
+                    <p class="descriptionCopyBody"><?php echo $row_Resource['InLanguage']?$row_Resource['InLanguage']:'--'; ?></p>
                     <h5 class="descriptionCopyHeading">Date created</h5>
-                    <p class="descriptionCopyBody">date</p>
+                    <p class="descriptionCopyBody"><?php echo $row_Resource['DateCreated']?$row_Resource['DateCreated']:'--'; ?></p>
                 </div>
                 <div class="span3">
                     <h5 class="descriptionCopyHeading">Educational alignment</h5>
-                    <p class="descriptionCopyBody">Common core</p>
+                    <p class="descriptionCopyBody"><?php echo $row_Resource['EducationalAlignment']?$row_Resource['EducationalAlignment']:'--'; ?></p>
                     <h5 class="descriptionCopyHeading">Educational use</h5>
-                    <p class="descriptionCopyBody">Assignment</p>
+                    <p class="descriptionCopyBody"><?php echo $row_Resource['EducationalUse']?$row_Resource['EducationalUse']:'--'; ?></p>
                     <h5 class="descriptionCopyHeading">Time required</h5>
-                    <p class="descriptionCopyBody">1 hour 30 min</p>
+                    <p class="descriptionCopyBody"><?php echo $row_Resource['TimeRequired']?$row_Resource['TimeRequired']:'--'; ?></p>
                 </div>
                 <div class="span3">
                     <h5 class="descriptionCopyHeading">Intended audience</h5>
-                    <p class="descriptionCopyBody">Learner, teacher</p>
-                    <h5 class="descriptionCopyHeading">Copyright</h5>
-                    <p class="descriptionCopyBody"><a href="#" target="_blank">Usage rights</a></p>
+                    <p class="descriptionCopyBody"><?php echo $row_Resource['EndUserRole']; ?></p>
                 </div>         
             </section>
             
@@ -111,3 +168,6 @@
         <p></p> 
 </body>
 </html>
+<?php
+mysql_free_result($Resource);
+?>
