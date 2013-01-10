a5af1161d867773e4e9a1f7ceb940d9c42a2730c
Luke Byrne <luke.byrne@greatfridays.com>
2013-01-10 21:53:22 +0000

    got slider working using for cc
    got slider working using the cc connection instead of the projector
    connection

--- Connections/projector.php
+++ Connections/projector.php
@@ -2,7 +2,7 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_projector = "projectordev.cy8aybqmalrv.us-west-1.rds.amazonaws.com";
+$hostname_projector = "commoncore.cy8aybqmalrv.us-west-1.rds.amazonaws.com";
 $database_projector = "projector";
 $username_projector = "pearsonf";
 $password_projector = "Poseidon";
