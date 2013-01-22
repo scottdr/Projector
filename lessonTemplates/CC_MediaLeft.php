b2ab7c166c53a29770d7abb7f811c27d8bf9ff8b
Scott Richards <scott.srichards@gmail.com>
2013-01-22 14:54:30 -0800

    Adding Common Core templates.

---
+++ lessonTemplates/CC_MediaLeft.php
@@ -0,0 +1,6 @@
+<div id="FullWidthWrapper">
+  <div id="OpeningTwoColumnLeft"><?php GenerateImageTag(0); ?></div>
+  <div id="OpeningTwoColumnRight">
+  	<div id="Text"><?php echo $row_StepQuery['Text']; ?></div>
+  </div>
+</div>
---
+++ lessonTemplates/CC_MediaRight.php
@@ -0,0 +1,8 @@
+<div id="FullWidthWrapper">
+  <div id="MediaRightColumnLeft">
+  	<div id="Text"><?php echo $row_StepQuery['Text']; ?></div>
+  </div>
+  <div id="MediaRightColumnRight">
+	  <?php GenerateImageTag(0); ?>
+  </div>
+</div>
