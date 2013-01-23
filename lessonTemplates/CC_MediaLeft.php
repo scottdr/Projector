6204d0968655f01b23dc8e493df1937fc792a98c
tanyaheins <tanya.numen@gmail.com>
2013-01-23 07:52:20 -0500

    CC - Media Left template

--- lessonTemplates/CC_MediaLeft.php
+++ lessonTemplates/CC_MediaLeft.php
@@ -1,6 +1,9 @@
-<div id="FullWidthWrapper">
-  <div id="OpeningTwoColumnLeft"><?php GenerateImageTag(0); ?></div>
-  <div id="OpeningTwoColumnRight">
-  	<div id="Text"><?php echo $row_StepQuery['Text']; ?></div>
-  </div>
-</div>
+						<!-- MEDIA LEFT -->
+                        <div class="row-fluid">
+                         	<div class="span6 offset1">
+							<?php GenerateImageTag(0); ?>
+                            </div>
+                           	<div class="span4 lessonContent">
+								<?php echo $row_StepQuery['Text']; ?>
+                            </div><!-- /.span -->
+                        </div><!-- /.row fluid -->
