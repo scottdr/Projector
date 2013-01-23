194c302d823a32f2b8e6ecf584abda31820262c1
tanyaheins <tanya.numen@gmail.com>
2013-01-23 08:44:58 -0500

    CC - Text only

--- lessonTemplates/CC_TextOnly.php
+++ lessonTemplates/CC_TextOnly.php
@@ -1,6 +1,8 @@
 						<!-- TEXT ONLY -->
                         <div class="row-fluid">
-                            <div class="span6 offset3 lessonContent">
-								<?php echo $row_StepQuery['Text']; ?>
+                           	<div class="span6 offset lessonContent">
+                            	<div class="lesson-carousel-content">
+									<?php echo $row_StepQuery['Text']; ?>
+                                </div>
                             </div><!-- /.span -->
-                        </div><!-- /.row fluid -->
+                        </div><!-- /.row fluid -->                        
