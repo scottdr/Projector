0e0c98bc801d4bbe0ff4baf196c65c2c3ba54768
tanyaheins <tanya.numen@gmail.com>
2013-01-23 08:28:18 -0500

    CC - Media left css

--- lessonTemplates/CC_MediaLeft.php
+++ lessonTemplates/CC_MediaLeft.php
@@ -4,6 +4,8 @@
 							<?php GenerateImageTag(0); ?>
                             </div>
                            	<div class="span4 lessonContent">
-								<?php echo $row_StepQuery['Text']; ?>
+                            	<div class="lesson-carousel-content">
+									<?php echo $row_StepQuery['Text']; ?>
+                                </div>
                             </div><!-- /.span -->
                         </div><!-- /.row fluid -->
