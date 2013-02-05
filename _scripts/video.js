d3bce9f06ba9f320cabc2c0f8cb33ac601a54f1b
Stephen Downs <steve@plasticbrain.com>
2013-02-04 16:19:19 -0800

    Common Core: Fix for video element scripting to avoid multiple instances of video script injection.

--- _scripts/video.js
+++ _scripts/video.js
@@ -1,19 +1,37 @@
 // JavaScript Document
 
-
+// Attach any needed video-handling JavaScript for browsers incapable of handling video.
 function initVideo() {
+	var videoCSSUrl = "http://vjs.zencdn.net/c/video-js.css";
+	var videoScriptUrl = "http://vjs.zencdn.net/c/video.js";
+	
+	var videoScriptIsAttached = 0;
+	
 	if (!isVideoCapable()) {
 		// If the browser is not HTML 5 video element capable, then load desired Javascript video framework.
 		// For now, we use the Video.js (http://videojs.com) CDN.
-		var vidCSS = document.createElement("link");
-		vidCSS.href = "http://vjs.zencdn.net/c/video-js.css";
-		vidCSS.rel = "stylesheet";
-		document.getElementsByTagName("head")[0].appendChild(vidCSS);
-	  
-		var vidScript = document.createElement("script");
-		vidScript.type = "text/javascript";
-		vidScript.src = "http://vjs.zencdn.net/c/video.js";
-		document.getElementsByTagName("head")[0].appendChild(vidScript);
+		
+		// Test if video script has already been added.
+		// Do this by checking for the existence of the unique url of the auxiliary video.js css file.
+		var linkElements = Array.prototype.slice.call(document.getElementsByTagName("link"), 0);
+		for (var i = 0; i < linkElements.length; i++) {
+			if (linkElements[i].href == videoCSSUrl) {
+				videoScriptIsAttached = 1;
+			}
+		 }
+		
+		if (!videoScriptIsAttached) {
+		  var vidCSS = document.createElement("link");
+		  vidCSS.href = videoCSSUrl;
+		  vidCSS.rel = "stylesheet";
+		  document.getElementsByTagName("head")[0].appendChild(vidCSS);
+		
+		  var vidScript = document.createElement("script");
+		  vidScript.type = "text/javascript";
+		  vidScript.src = videoScriptUrl;
+		  document.getElementsByTagName("head")[0].appendChild(vidScript);
+		  _videoScriptAdded = 1;
+		}
 	}
 }
 
