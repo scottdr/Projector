f40fa84c5e32a9c458714d02d99a4d7945169751
Stephen Downs <steve@plasticbrain.com>
2013-02-05 16:53:09 -0800

    Common Core: MathJax script now handles MathML rendering for MathML incapable browsers.

--- _scripts/math.js
+++ _scripts/math.js
@@ -1,5 +1,19 @@
 // JavaScript Document
 
+// Add MathJax scripts via CDN only on browsers that can't handle display of MathML.
+if (!isMathMLCapable()) {
+	if ((window.unsafeWindow == null ? window : unsafeWindow).MathJax == null) {
+	  if ((document.getElementsByTagName("math").length > 0) ||
+		  (document.getElementsByTagNameNS == null ? false :
+		  (document.getElementsByTagNameNS("http://www.w3.org/1998/Math/MathML","math").length > 0))) {
+		var script = document.createElement("script");
+		script.type = "text/javascript";
+		script.src = "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=MML_HTMLorMML";
+		document.getElementsByTagName("head")[0].appendChild(script);
+	  }
+	}
+}
+
 function isMathMLCapable() {
 	var e = document.createElement('div');
     e.innerHTML = '<math></math>';
